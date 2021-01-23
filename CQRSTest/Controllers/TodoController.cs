60f234849c3013cdb8d9de5039bf3fbb34848dbe
dineshpriyantha <dinesh.priyantha.uom@gmail.com>
2021-01-24 00:48:52 +0900

    Add project files.

---
+++ CQRSTest/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using CQRSTest.Commands;
+using CQRSTest.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSTest.Controllers
+{
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public TodoController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet("/{id}")]
+        public async Task<IActionResult> GetTodoById(int id)
+        {
+            var response = await mediator.Send(new GetTodoById.Query(id));
+            return response == null ? NotFound() : Ok(response);
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddTodo(AddTodo.Command command) => Ok(await mediator.Send(command));
+    }
+}
---
+++ CQRSTest/Queries/GetTodoById.cs
@@ -0,0 +1,39 @@
+﻿using CQRSTest.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRSTest.Queries
+{
+    public static class GetTodoById
+    {
+        // Query / Command
+        // All the data we need to execute
+        public record Query(int Id) : IRequest<Response>;
+
+        // Handler
+        // All the business logic to execute. Returns a response.
+        public class Handler : IRequestHandler<Query, Response>
+        {
+            private readonly Repository repository;
+
+            public Handler(Repository repository)
+            {
+                this.repository = repository;
+            }
+
+            public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var todo = repository.Todos.FirstOrDefault(x => x.Id == request.Id);
+                return todo == null ? null : new Response(todo.Id, todo.Name, todo.Completed);
+            }
+        }
+
+        // Response
+        // The data we want to return
+        public record Response(int Id, string Name, bool Completed);
+    }
+}
